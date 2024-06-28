66ec036b7760f7f961a0d2cb3b503042e8027128
chenzhwsysu <ziweicen@icloud.com>
2024-06-28 17:18:35 +0800

    crawl script

---
+++ PubMed/script.sh
@@ -0,0 +1,31 @@
+python concurrent_crawlpubmed.py --keywords "narcotics" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "tramadol" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 720 &
+
+python concurrent_crawlpubmed.py --keywords "morphine" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "fentanyl" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "benzodiazepines" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "diazepam" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "midazolam" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "dexmedetomidine" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 971 &
+
+python concurrent_crawlpubmed.py --keywords "atropine" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 1000 &
+
+python concurrent_crawlpubmed.py --keywords "penehyclidine hydrochloride" --pdfdir "downloaded_pdfs/anaesthesiology/*.pdf" --startpage 1 --endpage 14 &
+
+# 以上脚本使用术语：
+# 镇痛药 (narcotics)
+# 曲马多 ( tramadol)
+# 吗啡 (morphine)
+# 芬太尼 (fentanyl)
+# 苯二氮革类药物 (benzodiazepines)
+# 地西洋 (diazepam,安定)
+# 咪 达嗤仑 (midazolam)
+# 右美托咪定 (dexme­ detomidine) 
+# 阿托品 (atropine) 
+# 盐酸戊乙奎酿 (penehyclidine hydrochloride) 
